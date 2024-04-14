0e8671df8f3a093d1d829b53942e90bd69714bdc
fajarudinzakariya <fajarudinzakariyaa8@gmail.com>
2024-04-14 23:18:00 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import LoginPage from "./Pages/login";
+import RegisterPage from "./Pages/register";
+import NotFoundPage from "./Pages/NotFound";
+import HomePage from "./Pages/Home";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/Home",
+    element: <HomePage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
